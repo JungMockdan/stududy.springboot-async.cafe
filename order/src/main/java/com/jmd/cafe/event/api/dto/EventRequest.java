2939b00e2fa410abdac9da41a05c201910f04fcb
jmd <yacksan1!>
2021-07-06 15:46:12 +0900

    haha Strategy pattern and Async Combination.

---
+++ order/src/main/java/com/jmd/cafe/event/api/dto/EventRequest.java
@@ -0,0 +1,10 @@
+package com.jmd.cafe.event.api.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class EventRequest {
+    private String id;
+}
---
+++ order/src/main/java/com/jmd/cafe/event/api/dto/EventResponse.java
@@ -0,0 +1,10 @@
+package com.jmd.cafe.event.api.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class EventResponse {
+    private String result;
+}
